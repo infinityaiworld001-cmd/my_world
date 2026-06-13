98970422b330dd9a5cb2fdaf847cbae8071df4ba
infinityaiworld001-cmd <infinityaiworld001@gmail.com>
2026-06-13 11:43:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -191,5 +191,22 @@ def manga_episode3(page):
         total_pages=total_pages
     )
 
+@app.route("/manga/episode-4/<int:page>")
+def manga_episode4(page):
+
+    total_pages = 16
+
+    if page < 1:
+        page = 1
+
+    if page > total_pages:
+        page = total_pages
+
+    return render_template(
+        "manga_episode4.html",
+        page=page,
+        total_pages=total_pages
+    )
+
 if __name__ == "__main__":
     app.run(debug=True)
